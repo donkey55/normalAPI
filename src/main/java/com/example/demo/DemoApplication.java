3f0ebb6035f805753f6f23bc3cad6316ff5af389
donkey55 <lvshuopeng@icloud.com>
2022-09-08 16:46:28 +0800

    fix: change yml

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,19 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @SpringBootApplication
 @MapperScan("com.neo.mapper")
 public class DemoApplication {
 
     public static void main(String[] args) {
-        Map<String, Integer> words = new HashMap<>();
-        words.put("hello", 3);
-        words.put("world", 4);
-        words.merge("hello", 1, Integer::sum);
-        System.out.println(words.get("hello"));
         SpringApplication.run(DemoApplication.class, args);
     }
 
